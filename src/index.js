6eaba3b9d108b789088187437757b75e5944ffcb
Mohammad Fitra Ashari <fitra.drive@gmail.com>
2020-09-01 12:46:58 +0700

    class dan static function

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
-//shorthand
-// name = "Fitra"
-// umur = 20
+// class
+class members{
+     constructor(name,umur){
+         //jika ingin mengakses variable didalam class gumakan this
+         this.name = name
+         this.umur = umur
+     }
+    getData(name,umur){
+        return `nama ${this.name} umur ${this.umur}`
+    }
 
-// getData=()=>{
-//     return `member ${name} umurnya ${umur}`
-// }
-
-// let member = {
-//     name,umur,getData
-// }
-// console.log(member.getData())
-
-//destructuring
-// let member ={
-//     name:'Fitra',
-//     umur:20
-// }
-
-// let{umur:age,name} = member
-// console.log(age)
-
-let umur = [1,2,3]
-let[a,b,c] = umur
-console.log(a+b+c)
+    static getPoint(){//static function bsia di panggil tanpa di instgansiasi
+        return `Pointnnya 100`
+    }
+}
+let member = new members('fitra',20)
+console.log(members.getPoint());

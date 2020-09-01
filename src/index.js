81361a4c9575e4072e72a1c6814ca0e00320034b
Mohammad Fitra Ashari <fitra.drive@gmail.com>
2020-09-01 12:37:08 +0700

    shorthand dan destructuring

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-let username = 'fitra'
-let umur = 20
-// let text = `Member ${username} umurnya ${umur}`
-// let div = `
-// <h1>${username}</h1>
-// <h1>anjay</h1>
-// `;
+//shorthand
+// name = "Fitra"
+// umur = 20
 
+// getData=()=>{
+//     return `member ${name} umurnya ${umur}`
+// }
 
+// let member = {
+//     name,umur,getData
+// }
+// console.log(member.getData())
 
-test = (strings, username,umur)=>{
-    let string1 = strings[0]
-    let string2 = strings[1]
-    console.log(umur);
-}
-let output = test`nama saya adalah ${username} umurnya ${umur}`
-// console.log(output);
+//destructuring
+// let member ={
+//     name:'Fitra',
+//     umur:20
+// }
+
+// let{umur:age,name} = member
+// console.log(age)
+
+let umur = [1,2,3]
+let[a,b,c] = umur
+console.log(a+b+c)

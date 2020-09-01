20b8c38f29dde6e09e1ab96ef1e8491296b1318b
Mohammad Fitra Ashari <fitra.drive@gmail.com>
2020-09-01 10:23:38 +0700

    default value

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
-// //syntax arrow => function shorthand
-
-// let members = ['fitra','ashari','arashi'];
-// // hanya 1 parameter 
-// // members.forEach(member=>console.log(member));
-
-// //jika menggunakan  parameter harus menggunakan kurung
-
-// //tidak perlu kurung kurawal jika tanpa return
-// let membersindex = members.map((member,index)=>
-//     member+' == '+index);
-// console.log(membersindex);
-
-let data={
-    members: ["fitra","ashari"],
-    getMembers(){
-        //ketika menggunakan syntax arrow this jadi objek itu sendiri, jika menggunakan function this = window
-        this.members.map((name)=>{
-            console.log(this);
-        })
-    }
+generateTitle=()=> {
+    return 'Judul Default'
 }
-data.getMembers();
+
+createTag = (judul=generateTitle(),tag='bebas') =>{
+    // tag = tag || 'tag'
+    console.log('Forum '+judul+' Punya tag #'+tag)
+}
+
+createTag()
